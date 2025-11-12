3d38b31acc65af231a43f2e21edcecc83e342092
firdaus12p <105841101320@student.unismuh.ac.id>
2025-11-12 17:13:28 +0800

    clean

--- src/pages/admin/AmbassadorForm.tsx
+++ src/pages/admin/AmbassadorForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import AdminLayout from "../../components/layout/AdminLayout";
 import { Card, Button } from "../../components";
 import {
--- src/pages/admin/Ambassadors.tsx
+++ src/pages/admin/Ambassadors.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import AdminLayout from "../../components/layout/AdminLayout";
 import { Card, Button, Badge } from "../../components";
 import {
