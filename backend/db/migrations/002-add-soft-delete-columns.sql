7bbca4c37f6547637feebc85ac23ffa486654674
firdaus12p <105841101320@student.unismuh.ac.id>
2025-12-19 15:30:06 +0800

    chore: remove sensitive files from repository - keep only in gitignore

--- backend/db/migrations/002-add-soft-delete-columns.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE affiliate_usage ADD COLUMN deleted_at TIMESTAMP NULL DEFAULT NULL COMMENT 'Timestamp when record was soft deleted (NULL = active)';
-
-ALTER TABLE affiliate_usage ADD COLUMN deleted_by VARCHAR(255) NULL DEFAULT NULL COMMENT 'Username or identifier of who deleted this record';
-
-ALTER TABLE affiliate_usage ADD INDEX idx_deleted_at (deleted_at);
-
-ALTER TABLE affiliate_usage ADD INDEX idx_ambassador_deleted (ambassador_id, deleted_at);
